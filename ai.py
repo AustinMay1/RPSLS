517b48167f91f486e8ba7d84393f08e3626eda1b
Alexander Messier <alexander.j.messier@gmail.com>
2021-09-10 13:59:50 -0500

    Scaffolded game.py and ai.py

---
+++ ai.py
@@ -0,0 +1,6 @@
+class Ai:
+    def __init__(self):
+        pass
+
+    def get_gesture(self):
+        pass
---
+++ game.py
@@ -0,0 +1,30 @@
+class Game:
+    def __init__(self):
+        pass
+
+    def display_rules(self):
+        pass
+
+    def is_opponent_human_or_ai(self):
+        pass
+
+    def play_round(self):
+        pass
+
+    def display_gestures(self):
+        pass
+
+    def user_select(self):
+        pass
+
+    def opponent_select(self):
+        pass
+
+    def display_round_winner(self):
+        pass
+
+    def track_score(self):
+        pass
+
+    def display_game_winner(self):
+        pass
