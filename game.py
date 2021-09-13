f838eb24d9f1783551ea31d3289c7bec3ad0d7fa
Alexander Messier <alexander.j.messier@gmail.com>
2021-09-13 11:10:42 -0500

    Added play_round and print_score methods to track score and eventually decide winner.

--- game.py
+++ game.py
@@ -10,7 +10,7 @@ class Game:
     def run_game(self):
         self.display_rules()
         self.is_opponent_human_or_ai()
-        self.play_round()
+        self.start_round()
         self.display_game_winner()
 
     def display_rules(self):
@@ -24,38 +24,76 @@ class Game:
         else:
             self.opponent = Human()
 
-    def play_round(self):
+    def start_round(self):
         user_name = self.user.get_name()
         print(user_name)
         opp_name = self.opponent.get_name()
         print(opp_name)
-        self.user_select()
-        self.opponent_select()
-        
+        while (self.user.score < 2 and self.opponent.score < 2):
+            user_select = self.user_select()
+            opp_select = self.opponent_select()
+            print(user_select, opp_select)
+            self.play_round(user_select, opp_select)
 
-    def display_gestures(self):
-        turn = self.user.get_gesture()
-        print(turn)
+    def play_round(self, user_select, opponent_select):
+        if user_select == 'rock' and opponent_select == 'scissors':
+            self.user.score += 1
+        elif user_select == 'scissors' and opponent_select == 'rock':
+            self.opponent.score += 1
+        elif user_select == 'scissors' and opponent_select == 'paper':
+            self.user.score += 1
+        elif user_select == 'paper' and opponent_select == 'scissors':
+            self.opponent.score += 1
+        elif user_select == 'paper' and opponent_select == 'rock':
+            self.user.score += 1
+        elif user_select == 'rock' and opponent_select == 'paper':
+            self.opponent.score += 1
+        elif user_select == 'rock' and opponent_select == 'lizard':
+            self.user.score += 1
+        elif user_select == 'lizard' and opponent_select == 'rock':
+            self.opponent.score += 1
+        elif user_select == 'lizard' and opponent_select == 'spock':
+            self.user.score += 1
+        elif user_select == 'spock' and opponent_select == 'lizard':
+            self.opponent.score += 1
+        elif user_select == 'spock' and opponent_select == 'scissors':
+            self.user.score += 1
+        elif user_select == 'scissors' and opponent_select == 'spock':
+            self.opponent.score += 1
+        elif user_select == 'scissors' and opponent_select == 'lizard':
+            self.user.score += 1
+        elif user_select == 'lizard' and opponent_select == 'scissors':
+            self.opponent.score += 1
+        elif user_select == 'lizard' and opponent_select == 'paper':
+            self.user.score += 1
+        elif user_select == 'paper' and opponent_select == 'lizard':
+            self.opponent.score += 1
+        elif user_select == 'paper' and opponent_select == 'spock':
+            self.user.score += 1
+        elif user_select == 'spock' and opponent_select == 'paper':
+            self.opponent.score += 1
+        elif user_select == 'spock' and opponent_select == 'rock':
+            self.user.score += 1
+        elif user_select == 'rock' and opponent_select == 'spock':
+            self.opponent.score += 1
+        else:
+            print('You tied!')
+        self.print_score()
 
     def user_select(self):
         turn = self.user.get_gesture()
-        print(turn)
-        
+        return turn
 
     def opponent_select(self):
-        if self.opponent.type == 'ai':
-            opponent_turn = self.opponent.get_gesture()
-            print(opponent_turn)
-        else:
-            opponent_turn = self.opponent.get_gesture()
-            print(opponent_turn)
-
+        turn = self.opponent.get_gesture()
+        return turn
 
     def display_round_winner(self):
         pass
 
-    def track_score(self):
-        pass
+    def print_score(self):
+        print(f'{self.user.name} has :{self.user.score} points.')
+        print(f'{self.opponent.name} has :{self.opponent.score} points.')
 
     def display_game_winner(self):
         pass
